af49d4e2979807d7994886b439a6ad815bb0fd64
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-09-08 00:18:35 +0300

    redux implementation

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    {/* </PersistGate> */}
+    {/* </Provider> */}
   </React.StrictMode>
 );
