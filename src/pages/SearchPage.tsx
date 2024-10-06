428f9d7691ee28a6d6933ff0eab417b7350b4f3c
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-10-06 13:07:52 +0300

    fixing module import

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,7 +3,7 @@ import MovieList from '../components/MovieList/MovieList';
 import toast, { Toaster } from 'react-hot-toast';
 import { getMovieList } from '../redux/movie/operations';
 import { useDispatch, useSelector } from 'react-redux';
-import { Trending } from '../components/Trending/Trending';
+import Trending from '../components/Trending/Trending';
 import { selectTrendingOption } from '../redux/movie/selectors';
 import SearchForm from '../components/SearchForm/SearchForm';
 import Welcome from '../components/Welcome/Welcome';
