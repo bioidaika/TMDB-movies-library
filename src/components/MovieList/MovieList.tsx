c52dc6f59d0a36aa84e4d67595a432e16fcbf332
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-10-12 21:25:14 +0300

    small fixes in MovieList component

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -27,24 +27,40 @@ const MovieList: FC<FilteredMovieProps> = ({ children }) => {
           {movieLister.map(item => (
             <li key={item.id} className={css.list_item}>
               <Link to={`/movies/${item.id}`} state={location}>
-                <img
-                  loading="lazy"
-                  src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`}
-                  srcSet={`https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path} 1x, https://media.themoviedb.org/t/p/w440_and_h660_face/${item.poster_path} 2x`}
-                  alt={item.title}
-                ></img>
+                {item.poster_path != null && (
+                  <img
+                    loading="lazy"
+                    src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`}
+                    srcSet={`https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path} 1x, https://media.themoviedb.org/t/p/w440_and_h660_face/${item.poster_path} 2x`}
+                    alt={item.title}
+                    className={css.img_poster}
+                  />
+                )}
+                {item.poster_path == null && (
+                  <img
+                    loading="lazy"
+                    src={`https://davooda.com/images/outline/outline-file-document-question-mark-icon_360_w.png`}
+                    alt={'Unknown image'}
+                    className={css.img_poster}
+                  />
+                )}
+
                 <div className={css.title}>{item.title}</div>
                 <div className={css.description}>
-                  <div className={css.details}>{item.release_date.substring(0, 4)}</div>
+                  <div className={css.details}>
+                    {item.release_date.length != 0 ? item.release_date.substring(0, 4) : null}
+                  </div>
+                  {item.release_date.length != 0 && item.genre_ids[0] != null && ','}
                   <div className={css.details}>
                     {/* {item.genre_ids.map(id =>
                       genresOBJ.map((item: { id: number; name: string }) =>
                         item.id == id ? item.name + ' ' : null
                       )
                     )} */}
-                    {genresOBJ.map((key: { id: number; name: string }) =>
-                      key.id == item.genre_ids[0] ? ', ' + key.name : null
-                    )}
+                    {item.genre_ids[0] != null &&
+                      genresOBJ.map((key: { id: number; name: string }) =>
+                        key.id == item.genre_ids[0] ? key.name : null
+                      )}
                   </div>
                 </div>
               </Link>
