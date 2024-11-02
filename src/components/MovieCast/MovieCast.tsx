59e1e3551c18099a7b121d0bfa66ffcf5240aa54
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-11-02 20:58:37 +0300

    update MovieCaste

--- src/components/MovieCast/MovieCast.tsx
+++ src/components/MovieCast/MovieCast.tsx
@@ -12,7 +12,6 @@ export default function MovieCast() {
   const [error, setError] = useState<boolean>(false);
   const { movieID } = useParams();
   const [selectedCast, setSelectedCast] = useState<ICast[] | null>(null);
-  // const defaultSRC = `https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg`;
   const defaultSRC = `https://assets.mycast.io/actor_images/actor-an-unknown-actor-465215_large.jpg`;
   useEffect(() => {
     async function getData() {
@@ -36,7 +35,6 @@ export default function MovieCast() {
 
   return (
     <div>
-      {/* {error && <b>HTTP error!</b>} */}
       <Toaster />
       {isLoading && <Loader />}
       {!isLoading && (
