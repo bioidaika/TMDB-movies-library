096068a086dd877f5fd6ad596620ee15a7d1d660
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2025-03-09 02:37:10 +0300

    loader styles updated

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,9 @@
 import { RotatingLines } from 'react-loader-spinner';
+import css from './Loader.module.css';
 
 export default function Loader() {
   return (
-    <div>
+    <div className={css.loader}>
       <RotatingLines
         visible={true}
         // height="55"
@@ -11,6 +12,7 @@ export default function Loader() {
         ariaLabel="rotating-lines-loading"
         strokeColor="#6488ea"
       />
+      <div>This may take up to 50 seconds due to server idle state.</div>
     </div>
   );
 }
