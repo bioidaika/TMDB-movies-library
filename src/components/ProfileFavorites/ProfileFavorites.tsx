40af623b71dffa1a92e44409f2c936219cf1ce11
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2025-03-18 01:15:27 +0300

    refactoring ProfileFavorites

--- src/components/ProfileFavorites/ProfileFavorites.tsx
+++ src/components/ProfileFavorites/ProfileFavorites.tsx
@@ -3,17 +3,20 @@ import { selectFavorite } from '../../redux/auth/selectors';
 import css from './ProfileFavorites.module.css';
 import { Link, useLocation } from 'react-router-dom';
 import genres from '../genres.json';
+import { useMemo } from 'react';
 
 export const ProfileFavorites: React.FC = () => {
-  const genresList = JSON.stringify(genres);
-  const genresOBJ = JSON.parse(genresList);
+  const genresOBJ = useMemo(() => {
+    return new Map(genres.map(g => [g.id, g.name]));
+  }, []);
+
   const favorites = useSelector(selectFavorite);
   const location = useLocation();
   return (
     <div>
       <h1 className={css.header}>Saved Movies & TV Shows</h1>
+      {favorites && favorites.length === 0 && <div className={css.noResults}>No results</div>}
       <ul className={css.list}>
-        {favorites && favorites.length === 0 && <div>No results</div>}
         {favorites &&
           favorites.map(item => (
             <li key={item.media_id} className={css.list_item}>
@@ -23,55 +26,29 @@ export const ProfileFavorites: React.FC = () => {
                 }
                 state={location}
               >
-                {/*  refactoring start */}
-                {/* {item.poster_path != null && (
-                  <img
-                    loading="lazy"
-                    src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`}
-                    srcSet={`https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path} 1x, https://media.themoviedb.org/t/p/w440_and_h660_face/${item.poster_path} 2x`}
-                    alt={item.title || 'No title available'}
-                    className={css.img_poster}
-                  />
-                )}
-                {item.poster_path == null && (
-                  <img
-                    loading="lazy"
-                    src={`https://davooda.com/images/outline/outline-file-document-question-mark-icon_360_w.png`}
-                    alt={'Unknown image'}
-                    className={css.img_poster}
-                  />
-                )} */}
-                {item.poster_path != null ? (
-                  <img
-                    loading="lazy"
-                    src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`}
-                    srcSet={`https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path} 1x, https://media.themoviedb.org/t/p/w440_and_h660_face/${item.poster_path} 2x`}
-                    alt={item.title || 'No title available'}
-                    className={css.img_poster}
-                  />
-                ) : (
-                  <img
-                    loading="lazy"
-                    src={`https://davooda.com/images/outline/outline-file-document-question-mark-icon_360_w.png`}
-                    alt={'Unknown image'}
-                    className={css.img_poster}
-                  />
-                )}
-                {/*  refactoring end */}
+                <img
+                  loading="lazy"
+                  src={
+                    item.poster_path
+                      ? `https://media.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`
+                      : 'https://davooda.com/images/outline/outline-file-document-question-mark-icon_360_w.png'
+                  }
+                  alt={item.title || 'No title available'}
+                  className={css.img_poster}
+                />
                 <div className={css.title}>{item.title}</div>
                 <div className={css.description}>
-                  <div className={css.details}>
-                    {item.release_date ? item.release_date.substring(0, 4) : null}
-                  </div>
-                  {item.genres && item.genres[0] != null && item.release_date && (
-                    <span> ,&nbsp;</span>
+                  {item.release_date && (
+                    <div className={css.details}>{item.release_date.slice(0, 4)}</div>
                   )}
-                  <div className={css.details}>
-                    {item.genres &&
-                      genresOBJ.map((key: { id: number; name: string }) =>
-                        key.id === (item.genres?.[0] ?? -1) ? key.name : null
-                      )}
-                  </div>
+                  {item.genres && item.genres.length > 0 && (
+                    <>
+                      <span className={css.separator}>,&nbsp;</span>
+                      <div className={css.details}>
+                        {genresOBJ.get(item.genres[0]) || 'Unknown Genre'}
+                      </div>
+                    </>
+                  )}
                 </div>
               </Link>
             </li>
