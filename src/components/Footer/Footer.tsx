538ef88df5530ecc0dcd75a159c818e5ce224f29
bioidaika <ost.nostalgia@gmail.com>
2025-04-24 12:54:20 +0700

    improve cache

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,21 @@
 import css from './Footer.module.css';
 import { memo } from 'react';
-import { FaLinkedin } from 'react-icons/fa6';
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaDiscord } from 'react-icons/fa'; // Import icon Discord
+import { SiZalo } from 'react-icons/si'; // Import icon Zalo
 
 const Footer = memo(() => {
   return (
     <footer className={css.footer}>
       <div className={css.container}>
+        {/* Thông tin về ứng dụng */}
         <div className={css.about}>
-          <p className={css.text}>Created by Dmytrii Tsybuliak</p>
-          <p className={css.text}>Full Stack Developer | Passionate about Web Development</p>
+          <p className={css.text}>Liên hệ hợp tác, mua điểm thưởng Fshare</p>
         </div>
 
+        {/* Liên kết mạng xã hội */}
         <div className={css.socialLinks}>
           <a
-            href="https://github.com/DmytriiTsybuliak"
+            href="https://github.com/bioidaika"
             target="_blank"
             rel="noopener noreferrer"
             className={css.link}
@@ -23,19 +24,29 @@ const Footer = memo(() => {
             <FaGithub className={css.social_media} />
           </a>
           <a
-            href="https://www.linkedin.com/in/dmytrii-tsybuliak-9a6320155/"
+            href="https://zalo.me/0942745244"
             target="_blank"
             rel="noopener noreferrer"
             className={css.link}
           >
-            {/* LinkedIn */}
-            <FaLinkedin className={css.social_media} />
+            {/* Zalo */}
+            <SiZalo className={css.social_media} />
+          </a>
+          <a
+            href="https://discord.gg/9Pa8A9ssKA"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css.link}
+          >
+            {/* Discord */}
+            <FaDiscord className={css.social_media} />
           </a>
         </div>
       </div>
 
+      {/* Bản quyền */}
       <div className={css.copyright}>
-        <p>© {new Date().getFullYear()} Dmytrii Tsybuliak. All Rights Reserved.</p>
+        <p>© {new Date().getFullYear()} Bioidaika. All Rights Reserved.</p>
       </div>
     </footer>
   );
